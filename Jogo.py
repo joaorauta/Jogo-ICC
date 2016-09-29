12ba0bbb657a73235fe4e6dc1adca2a80dc948d3
Rafael Teixeira <rodriguesteixeirarafael@gmail.com>
2016-09-29 06:02:45 -0300

    v1.0

--- Jogo.py
+++ Jogo.py
@@ -22,8 +22,8 @@ class Jogo():
 		self.fundo = Image(Point(375,225), "images/fundo.png")
 		self.fundo.draw(self.win)
 		
-		self.lutador = Lutador(100, 0, 90, 310, "ken")
-		self.lutador2 = Lutador(100, 0, 500, 310, "ken")
+		self.lutador = Lutador(300, 0, 90, 310, "ken")
+		self.lutador2 = Lutador(300, 0, 500, 310, "sagat2")
 
 		self.imgLutador1 = Image(Point(self.lutador.getX(), self.lutador.getY()), self.lutador.getImage())
 		self.imgLutador1.draw(self.win)
@@ -32,8 +32,36 @@ class Jogo():
 		self.distance = Line(self.imgLutador1.getAnchor(), self.imgLutador2.getAnchor())
 		self.limit = self.imgLutador1.getWidth()/2
 
+		self.healthBar1 = Rectangle(Point(20, 20), Point(320, 50))
+		self.healthBar1.setOutline("white")
+		self.healthBar1.setFill("yellow")
+		self.healthBar1.draw(self.win)
+
+		self.healthBar2 = Rectangle(Point(430, 20), Point(730, 50))
+		self.healthBar2.setOutline("white")
+		self.healthBar2.setFill("yellow")
+		self.healthBar2.draw(self.win)
+
+		self.redIncrement1 = 0
+		self.redBar1 = Rectangle(Point(20, 20), Point(20 + self.redIncrement1, 50))
+		self.redBar1.setOutline("white")
+		self.redBar1.setFill("red")
+
+		self.redIncrement2 = 0
+		self.redBar2 = Rectangle(Point(430, 20), Point(430 + self.redIncrement2, 50))
+		self.redBar2.setOutline("white")
+		self.redBar2.setFill("red")
+
 	def update(self):
-		
+		if(self.lutador.isDead()):
+			print "Jogador 2 venceu!"
+			print "console tosco paliativo"
+			self.stop()
+
+		if(self.lutador2.isDead()):
+			print "Jogador 1 venceu!"
+			print "console tosco paliativo"
+			self.stop()
 		#	Alterei a classe graphics, agora o metodo checkKey retorna uma lista com as teclas pressionadas.
 		#	A classe graphics agora trata mais dois eventos.
 		#	<KeyPress>:
@@ -46,8 +74,7 @@ class Jogo():
 		#	Rael me ajudou bastante com isso
 		
 		self.tecla = self.win.checkKey()
-		print self.tecla
-
+		
 		curr_time = time.clock()
 		
 		if("Escape" not in self.tecla):
@@ -58,17 +85,30 @@ class Jogo():
 				self.lutador.walk_right()
 			if("a" in self.tecla):
 				self.lutador.walk_left()
+			if("s" in self.tecla):
+				self.lutador.crouch()
+
+			if("q" in self.tecla):
+				self.lutador.punch()
+			if("e" in self.tecla):
+				self.lutador.kick()
 			if("w" in self.tecla):
-				if(not self.lutador.isJumping()):
-					self.lutador.jump(curr_time)
-			
+				self.lutador.block()
+
 			#========== LUTADOR 2 ==========#
 			
 			if("Left" in self.tecla):
 				self.lutador2.walk_left()			
 			if("Right" in self.tecla):
 				self.lutador2.walk_right()
-				
+			if("Down" in self.tecla):
+				self.lutador2.crouch()
+			if("b" in self.tecla):
+				self.lutador2.punch()
+			if("m" in self.tecla):
+				self.lutador2.kick()
+			if("n" in self.tecla):
+				self.lutador2.block()
 		else:
 			self.stop()
 	
@@ -77,7 +117,7 @@ class Jogo():
 		self.lutador2.move()
 
 	def checkColisions(self):
-
+		#300hp/damage
 		# Se a distancia entre o centro da linha entre os dois jogadores for menor
 		# que o centro do comeco da linha + a largura do jogador1/2 houve colisao!
 		# enquanto o pulo nao estiver pronto calculamos apenas essas distancias em X
@@ -86,46 +126,86 @@ class Jogo():
 		img_center = self.distance.getP1().getX()
 		center = distance_center - img_center	
 
-		#verifica qual dos dois esta na frente
-		if(self.lutador.getX() <= self.lutador2.getX()):
-			if(center <= self.limit):
-				if("punch2" in self.lutador.getImage()):
-					print "SOCO"
-		else:
-			if(center > self.limit):
-				if("punch2" in self.lutador.getImage()):
-					print "SOCO"
+		
+		if(center <= self.limit):
+			#colisao com as paredes
+			if(self.lutador.getDx() > 0):
+				self.lutador.setDx(0)
+			if(self.lutador2.getDx() < 0):
+				self.lutador2.setDx(0)
+
+			#======= SOCO =======#
+
+			if("punch2" in self.lutador.getImage()):
+				if(self.lutador2.isBlocking()):
+					self.lutador2.getHit(15/4, True)
+				else:
+					self.lutador2.getHit(15, False)
+
+			if("punch2" in self.lutador2.getImage()):
+				if(self.lutador.isBlocking()):
+					self.lutador.getHit(15/4, True)
+				else:
+					self.lutador.getHit(15, False)
+			
+			#======= CHUTE =======#
+
+			if("kick2" in self.lutador.getImage()):
+				if(self.lutador2.isBlocking()):
+					self.lutador2.getHit(30/4, True)
+				else:
+					self.lutador2.getHit(30, False)
+
+			if("kick2" in self.lutador2.getImage()):
+				if(self.lutador.isBlocking()):
+					self.lutador.getHit(30/4, True)
+				else:
+					self.lutador.getHit(30, False)
+		
 			
 	def redraw(self):
+		self.redIncrement1 = 300 - self.lutador.getHp()
+		self.redIncrement2 = 300 - self.lutador2.getHp()
 
+		self.redBar1 = Rectangle(Point(20, 20), Point(20 + self.redIncrement1, 50))
+		self.redBar1.setOutline("white")
+		self.redBar1.setFill("red")
+
+		self.redBar2 = Rectangle(Point(430, 20), Point(430 + self.redIncrement2, 50))
+		self.redBar2.setOutline("white")
+		self.redBar2.setFill("red")
+
+		self.redBar1.undraw()
+		self.redBar2.undraw()
+		
 		self.lutador.animate(self.lutador.getImage())
 		self.lutador2.animate(self.lutador2.getImage())
 		
 		self.fundo.undraw()
 		self.imgLutador1.undraw()
 		self.imgLutador2.undraw()
-		#self.distance.undraw()
 		
-		if("punch2" in self.lutador.getImage()):
-			#correcao em X porque a largura do sprite do soco eh diferente
-			self.imgLutador1 = Image(Point(self.lutador.getX()+15, self.lutador.getY()), self.lutador.getImage())
-		else:
-			self.imgLutador1 = Image(Point(self.lutador.getX(), self.lutador.getY()), self.lutador.getImage())
+		self.healthBar1.undraw()
+		self.healthBar2.undraw()
 		
-		if("punch2" in self.lutador2.getImage()):
-			#correcao em X porque a largura do sprite do soco eh diferente
-			self.imgLutador2 = Image(Point(self.lutador2.getX()+15, self.lutador2.getY()), self.lutador2.getImage())
-		else:
-			self.imgLutador2 = Image(Point(self.lutador2.getX(), self.lutador2.getY()), self.lutador2.getImage())
-	
-		self.imgLutador2= Image(Point(self.lutador2.getX(), self.lutador2.getY()), self.lutador2.getImage())
+
+		self.imgLutador1 = Image(Point(self.lutador.getX(), self.lutador.getY()), self.lutador.getImage())
+		self.imgLutador2 = Image(Point(self.lutador2.getX(), self.lutador2.getY()), self.lutador2.getImage())
+
 		self.distance = Line(self.imgLutador1.getAnchor(), self.imgLutador2.getAnchor())
-		self.limit = self.imgLutador1.getWidth()/2
+		self.limit = self.imgLutador2.getWidth()/2
 
 		self.fundo.draw(self.win)
+		
 		self.imgLutador1.draw(self.win)
 		self.imgLutador2.draw(self.win)
-		#self.distance.draw(self.win)
+		
+		self.healthBar1.draw(self.win)
+		self.healthBar2.draw(self.win)
+
+		self.redBar1.draw(self.win)
+		self.redBar2.draw(self.win)
+
 		self.win.update()
 
 	def stop(self):
--- Lutador.py
+++ Lutador.py
@@ -5,6 +5,8 @@ from graphics import *
 class Lutador():
 
 	def __init__(self, hp, mp, x, y, name):
+		self.name = name
+
 		self.hp = hp
 		self.mp = mp
 		
@@ -15,21 +17,43 @@ class Lutador():
 		self.vy = 3
 		self.g = -9.8
 
+
 		self.images = {}
-		self.images['idle'] = "images/" + name + "/idle1.png"
-		self.images['right'] = "images/" + name + "/idle1.png"
-		self.images['left'] = "images/" + name + "/idle1.png"
-		self.images['up'] = "images/" + name + "/idle1.png"
-		self.images['down'] = "images/" + name + "/idle1.png"
+		self.images['idle']   = "images/" + name + "/idle1.png"
+		self.images['right']  = "images/" + name + "/idle1.png"
+		self.images['left']   = "images/" + name + "/idle1.png"
+		self.images['up'] 	  = "images/" + name + "/idle1.png"
+		self.images['crouch']   = "images/" + name + "/crouch.png"
+		
+		self.images['block'] = "images/" + name + "/block.png"
+		self.images['c_block'] = "images/" + name + "/c_block.png"
 
 		self.images['punch1'] = "images/" + name + "/punch1.png"
 		self.images['punch2'] = "images/" + name + "/punch2.png"
 		self.images['punch3'] = "images/" + name + "/punch3.png"
 
+		self.images['c_punch1'] = "images/" + name + "/c_punch1.png"
+		self.images['c_punch2'] = "images/" + name + "/c_punch2.png"
+		self.images['c_punch3'] = "images/" + name + "/c_punch3.png"
+
+		self.images['kick1'] = "images/" + name + "/kick1.png"
+		self.images['kick2'] = "images/" + name + "/kick2.png"
+		self.images['kick3'] = "images/" + name + "/kick3.png"
+
+		self.images['c_kick1'] = "images/" + name + "/c_kick1.png"
+		self.images['c_kick2'] = "images/" + name + "/c_kick2.png"
+		self.images['c_kick3'] = "images/" + name + "/c_kick3.png"
+
 		self.images['idle1'] = "images/" + name + "/idle1.png"
 		self.images['idle2'] = "images/" + name + "/idle2.png"
 		self.images['idle3'] = "images/" + name + "/idle3.png"
 		self.images['idle4'] = "images/" + name + "/idle4.png"
+
+		self.images['hit1'] = "images/" + name + "/hit1.png"
+		self.images['hit2'] = "images/" + name + "/hit2.png"
+		self.images['hit3'] = "images/" + name + "/hit3.png"
+		self.images['hit4'] = "images/" + name + "/hit4.png"
+		self.images['c_hit'] = "images/" + name + "/c_hit.png"
 
 		self.images['walk1'] = "images/" + name + "/walk1.png"
 		self.images['walk2'] = "images/" + name + "/walk2.png"
@@ -44,9 +68,14 @@ class Lutador():
 
 		self.jumping = False
 		self.punching = False
+		self.kicking = False
 		self.moving = False
 		self.walking = False
-		
+		self.hit = False
+		self.blocking = False
+		self.crouching = False
+		self.dead = False
+
 		hitbox_p1X = (self.getX() - 40)
 		hitbox_p1Y = (self.getY() + 100)
 
@@ -55,13 +84,18 @@ class Lutador():
 
 		self.hitbox = Rectangle(Point(hitbox_p1X, hitbox_p1Y), Point(hitbox_p2X, hitbox_p2Y))
 
+	def die(self):
+		self.dead = True
 
 	def getX(self):
 		return self.x
 
+	def getDx(self):
+		return self.dx
+
 	def getY(self):
 		return self.y
-    
+
 	def getImage(self):
 		return self.image
 	
@@ -74,28 +108,96 @@ class Lutador():
 	def isPunching(self):
 		return self.punching
 
+	def isHit(self):
+		return self.hit
+
+	def getHp(self):
+		return self.hp
+
+	def isKicking(self):
+		return self.kicking
+
+	def isBlocking(self):
+		return self.blocking
+
+	def isCrouchinf(self):
+		return self.crouching
+
+	def isDead(self):
+		return self.dead
+
+	def setDx(self, dx):
+		self.dx = dx
+
 	def setX(self, x):
 		self.x = x
-    
+
 	def setY(self, y):
 		self.y = y    
         
 	def idle(self):
 		self.image = self.images['idle1']
 
+	def block(self):
+		self.walking = False
+		self.blocking = True
+		if(self.crouching):
+			self.image = self.images['c_block']
+		else:
+			self.image = self.images['block']
+		
+	def getHit(self, damage, blocking):
+		name_len = len(self.name)
+
+		#Se for o jogador2, move para a direita
+		if(self.name[name_len-1] == "2"):
+			if(not blocking):
+				self.dx = 10
+		else:
+			if(not blocking):
+				self.dx = -10
+		
+		if(self.crouching):
+			self.image = self.images['c_hit']
+		else:
+			self.image = self.images['hit1']
+
+		self.walking = False
+		self.hit = True
+
+		self.hp -= damage
+		if(self.hp <= 0):
+			self.die()
+
 	def walk_right(self):
-		if(not self.isPunching()):
+
+		if(self.crouching):
+			self.crouching = False
+			self.y -=30
+			
+
+		if(not self.isPunching() and not self.isBlocking()):
 			self.dx = 10
 			if("walk" not in self.image):
 				self.image = self.images['walk1']
 			self.walking = True
 
 	def walk_left(self):
-		if(not self.isPunching()):
+		if(self.crouching):
+			self.crouching = False
+			self.y -=30			
+
+		if(not self.isPunching() and not self.isBlocking() and not self.crouching):
 			self.dx = -10
 			if("walk" not in self.image):
 				self.image = self.images['walk1']
 			self.walking = True
+
+	def crouch(self):
+		if(not self.crouching):
+			self.crouching = True
+			self.y += 30
+		self.image = self.images['crouch']
 
 	def move(self):
 		distX = self.x + self.dx
@@ -103,8 +205,12 @@ class Lutador():
 		if(distX >= 0  and distX <= 750):
 			self.x = distX
 		self.y = self.y + self.dy
-		self.dx = 0
+		
+		if(not self.isHit()):
+			self.dx = 0
+
 		self.moving = False
+		
 
 	def jump(self, start_time):
 		pass
@@ -112,21 +218,50 @@ class Lutador():
 		
 
 	def punch(self):
-		self.walking = False
-		self.punching = True
-		self.image = self.images['punch1']
+		if(not self.isBlocking()):
+			self.walking = False
+			self.punching = True
+			if(self.crouching):
+				self.image = self.images['c_punch1']
+			else:
+				self.image = self.images['punch1']
+
+	def kick(self):
+		if(not self.isBlocking()):
+			self.walking = False
+			self.kicking = True
+			if(self.crouching):
+				self.image = self.images['c_kick1']
+			else:
+				self.image = self.images['kick1']
 
 	def animate(self, image):
-		if(self.punching):
-			if(image == self.images['punch1']):
-				self.image = self.images['punch2']
-				
-			if(image == self.images['punch2']):
-				self.image = self.images['punch3']
+		#print image
+		if(self.blocking):
+			self.blocking = False			
 
-			if(image == self.images['punch3']):
-				self.image = self.images['idle1']
-				self.punching = False
+		if(self.punching):
+			if(self.crouching):
+				if(image == self.images['c_punch1']):
+					self.image = self.images['c_punch2']
+					
+				if(image == self.images['c_punch2']):
+					self.image = self.images['c_punch3']
+
+				if(image == self.images['c_punch3']):
+					self.image = self.images['crouch']
+					self.punching = False
+
+			else:
+				if(image == self.images['punch1']):
+					self.image = self.images['punch2']
+					
+				if(image == self.images['punch2']):
+					self.image = self.images['punch3']
+
+				if(image == self.images['punch3']):
+					self.image = self.images['idle1']
+					self.punching = False
 
 		if(self.walking):
 			if(image == self.images['idle1']):
@@ -148,7 +283,52 @@ class Lutador():
 				self.image = self.images['idle1']
 				self.walking = False
 		
+		if(self.kicking):
+			if(self.crouching):
+				if(image == self.images['c_kick1']):
+					self.image = self.images['c_kick2']
+					
+				if(image == self.images['c_kick2']):
+					self.image = self.images['c_kick3']
+					#self.x+=60
+
+				if(image == self.images['c_kick3']):
+					self.image = self.images['crouch']
+					#self.x-=60
+					self.kicking = False
+			else:
+				if(image == self.images['kick1']):
+					self.image = self.images['kick2']
+					
+				if(image == self.images['kick2']):
+					self.image = self.images['kick3']
+
+				if(image == self.images['kick3']):
+					self.image = self.images['idle1']
+					self.kicking = False
+		
+		if(self.hit):
+			if(self.crouching):
+				self.image = self.images['crouch']
+				self.dx = 0
+				self.hit = False
+			else:
+				if(image == self.images['hit1']):
+					self.image = self.images['hit2']
+
+				if(image == self.images['hit2']):
+					self.image = self.images['hit3']
+					
+				if(image == self.images['hit3']):
+					self.image = self.images['hit4']
+		
+				if(image == self.images['hit4']):
+					self.image = self.images['idle1']
+					self.dx = 0
+					self.hit = False
+
 		else:
+
 			if(image == self.images['idle1']):
 				self.image = self.images['idle2']
 
