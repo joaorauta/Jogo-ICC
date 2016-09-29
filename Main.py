ebce816b1869b7c125c71410476453e49b88c05f
Rafael Teixeira <rodriguesteixeirarafael@gmail.com>
2016-09-29 06:07:29 -0300

    test

---
+++ Main.py
@@ -0,0 +1,26 @@
+from graphics import *
+from Jogo import *
+from Lutador import *
+import time
+import Tkinter
+
+j = Jogo()
+j.start()
+
+FPS = 30
+update_time = 1.0/FPS
+
+while j.getState():
+
+	start_time = time.time()
+
+	j.update()
+	j.redraw()
+
+	loop_time = time.time() - start_time
+	
+	if(loop_time < update_time):
+		time.sleep(update_time - loop_time)
+	
+	
+j.stop()
